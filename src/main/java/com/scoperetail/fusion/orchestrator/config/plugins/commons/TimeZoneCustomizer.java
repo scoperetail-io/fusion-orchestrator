69ff142bd0ac13064e8500a2c5d990963850d658
Javier Casanova Scoperetail <javier.casanova@scoperetail.com>
2021-05-13 19:42:19 -0500

    Timezone handling for OrderDropEvent, HashKey updated in templates

--- src/main/java/com/scoperetail/fusion/orchestrator/config/plugins/commons/TimeZoneCustomizer.java
+++ src/main/java/com/scoperetail/fusion/orchestrator/config/plugins/commons/TimeZoneCustomizer.java
@@ -5,31 +5,23 @@ import com.scoperetail.commons.time.util.DateTimeZoneUtil;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Collections;
-import java.util.Map;
 
 public final class TimeZoneCustomizer {
   private static final String TIMEZONE_UTIL = "TIMEZONE_UTIL";
+  private final static String GMT = "GMT";
   private final static String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
 
-  private TimeZoneCustomizer() {
+  public TimeZoneCustomizer() {
   }
 
-  public static Map<String, Object> getTimeZoneParams() {
-    return Collections.singletonMap(TIMEZONE_UTIL, new TimeZoneUtil());
-  }
-
-  public static class TimeZoneUtil {
-    public String toTimeZone(final String timezone, final String dateTime){
-      ZoneId fromZone = ZoneId.of("GMT");
-      ZoneId toZone = ZoneId.of(timezone.trim());
-      DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
-      LocalDateTime time = LocalDateTime.parse(dateTime, dateTimeFormatter);
-      LocalDateTime result = DateTimeZoneUtil.toLocalDateTime(
-          time,
-          fromZone,
-          toZone);
-      return result.format(dateTimeFormatter);
-    }
+  public String toTimeZone(final String timezone, final String dateTime){
+    ZoneId fromZone = ZoneId.of(GMT);
+    ZoneId toZone = ZoneId.of(timezone.trim());
+    DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+    LocalDateTime result = DateTimeZoneUtil.toLocalDateTime(
+        LocalDateTime.parse(dateTime, dateTimeFormatter),
+        fromZone,
+        toZone);
+    return result.format(dateTimeFormatter);
   }
 }
