59531708325b525dd688703b4ae7f7b1d379ffcd
Javier Casanova Scoperetail <javier.casanova@scoperetail.com>
2021-05-13 19:47:09 -0500

    Fix last check in

--- src/main/java/com/scoperetail/fusion/orchestrator/adapter/in/web/command/OrderCommandDelegate.java
+++ src/main/java/com/scoperetail/fusion/orchestrator/adapter/in/web/command/OrderCommandDelegate.java
@@ -5,7 +5,6 @@ import static org.springframework.http.HttpStatus.ACCEPTED;
 import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
-import com.scoperetail.commons.time.util.DateTimeZoneUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,15 +19,10 @@ import com.scoperetail.fusion.orchestrator.domain.OrderDropEvent;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-
 @RestController
 @AllArgsConstructor
 @Slf4j
 public class OrderCommandDelegate implements OrdersApiDelegate {
-	private final static String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
 	private PosterUseCase posterUseCase;
 	private DedupeJpaAdapter dedupeJpaAdapter;
 
@@ -37,7 +31,6 @@ public class OrderCommandDelegate implements OrdersApiDelegate {
 		HttpStatus result = CONFLICT;
 		if (isNotDuplicate(orderDropEventRequest)) {
 			try {
-				datesTimeZone(orderDropEventRequest);
 				posterUseCase.post(OrderDropEvent, orderDropEventRequest, true);
 				result = ACCEPTED;
 			} catch (final Exception e) {
@@ -61,24 +54,5 @@ public class OrderCommandDelegate implements OrdersApiDelegate {
 		response.setMessage(httpStatus.getReasonPhrase());
 		response.setType(httpStatus.name());
 		return ResponseEntity.status(httpStatus).body(response);
-	}
-
-	private void datesTimeZone(final OrderDropEvent orderDropEventRequest){
-		ZoneId fromZone = ZoneId.of("GMT");
-		String timezone = orderDropEventRequest.getRoutingInfo().getSourceNode().getLocation().getTimezone().trim();
-		ZoneId toZone = ZoneId.of(timezone);
-		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
-
-		LocalDateTime time = LocalDateTime.parse(orderDropEventRequest.getOrder().getFulfillmentOrder().getEarliestPickTime(), dateTimeFormatter);
-		LocalDateTime result = DateTimeZoneUtil.toLocalDateTime(time, fromZone, toZone);
-		orderDropEventRequest.getOrder().getFulfillmentOrder().setEarliestPickTime(result.format(dateTimeFormatter));
-
-		time = LocalDateTime.parse(orderDropEventRequest.getOrder().getFulfillmentOrder().getPickDueTime(), dateTimeFormatter);
-		result = DateTimeZoneUtil.toLocalDateTime(time, fromZone, toZone);
-		orderDropEventRequest.getOrder().getFulfillmentOrder().setPickDueTime(result.format(dateTimeFormatter));
-
-		time = LocalDateTime.parse(orderDropEventRequest.getOrder().getFulfillmentOrder().getExpectedOrderPickupTime(), dateTimeFormatter);
-		result = DateTimeZoneUtil.toLocalDateTime(time, fromZone, toZone);
-		orderDropEventRequest.getOrder().getFulfillmentOrder().setExpectedOrderPickupTime(result.format(dateTimeFormatter));
 	}
 }
