e279479bbf25fb62eca87057f3b0fdff2cd66223
Javier Casanova Scoperetail <javier.casanova@scoperetail.com>
2021-05-17 17:28:20 -0500

    Vault implementation for security config

--- src/main/java/com/scoperetail/fusion/orchestrator/config/SecurityConfig.java
+++ src/main/java/com/scoperetail/fusion/orchestrator/config/SecurityConfig.java
@@ -1,7 +1,8 @@
+/* ScopeRetail (C)2021 */
 package com.scoperetail.fusion.orchestrator.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,27 +13,39 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableConfigurationProperties(VaultConfig.class)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	@Value("${spring.security.user.name:97ce9cc8-8db8-4040-9850-3aed0a95cf06}")
-	private String userName;
+  private final VaultConfig vaultConfig;
 
-	@Value("${spring.security.user.password:*Ag#E6MAUHpW8vGL7@$Z}")
-	private String userPassword;
+  public SecurityConfig(VaultConfig vaultConfig) {
+    super();
+    this.vaultConfig = vaultConfig;
+  }
 
-	@Autowired
-	public void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser(userName).password(passwordEncoder().encode(userPassword)).roles("USER");
-	}
+  @Autowired
+  public void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+        .withUser(vaultConfig.getUsername())
+        .password(passwordEncoder().encode(vaultConfig.getPassword()))
+        .roles("USER");
+  }
 
-	@Override
-	protected void configure(final HttpSecurity http) throws Exception {
-		http.csrf().disable().httpBasic().and().authorizeRequests().antMatchers("/actuator/**").permitAll().anyRequest()
-				.authenticated();
-	}
+  @Override
+  protected void configure(final HttpSecurity http) throws Exception {
+    http.csrf()
+        .disable()
+        .httpBasic()
+        .and()
+        .authorizeRequests()
+        .antMatchers("/actuator/**")
+        .permitAll()
+        .anyRequest()
+        .authenticated();
+  }
 
-	// Spring boot needs BCryptPasswordEncoder for encoding user passwords.
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+  // Spring boot needs BCryptPasswordEncoder for encoding user passwords.
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
