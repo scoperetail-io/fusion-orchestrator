d82595775e1d3e94a14eeb5f3ece35c06b38f45d
Javier Rivera Casanova <javier@Javiers-MacBook-Pro.local>
2021-05-26 14:11:37 -0500

    Fix garbage characters when gzip

--- src/main/java/com/scoperetail/fusion/orchestrator/adapter/out/web/RetryRestServiceImpl.java
+++ src/main/java/com/scoperetail/fusion/orchestrator/adapter/out/web/RetryRestServiceImpl.java
@@ -1,9 +1,11 @@
 package com.scoperetail.fusion.orchestrator.adapter.out.web;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,7 +15,9 @@ public class RetryRestServiceImpl implements RetryRestService {
   @Override
   public ResponseEntity<String> retryPost(
       String url, String methodType, HttpEntity<String> httpEntity) {
-    final RestTemplate restTemplate = new RestTemplate();
+    HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(
+        HttpClientBuilder.create().build());
+    final RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory);
     try {
       return restTemplate.exchange(url, HttpMethod.valueOf(methodType), httpEntity, String.class);
     } catch (Exception e) {
