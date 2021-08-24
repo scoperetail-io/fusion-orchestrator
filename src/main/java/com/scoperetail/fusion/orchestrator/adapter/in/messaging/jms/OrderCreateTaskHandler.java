22e2df677e799379a7270702fcea8a20cb040fc6
ravi-zadpide <ravi.zadpide@scoperetail.com>
2021-08-24 15:21:04 +0530

    Open source.

--- src/main/java/com/scoperetail/fusion/orchestrator/adapter/in/messaging/jms/OrderCreateTaskHandler.java
+++ src/main/java/com/scoperetail/fusion/orchestrator/adapter/in/messaging/jms/OrderCreateTaskHandler.java
@@ -1,6 +1,32 @@
 /* ScopeRetail (C)2021 */
 package com.scoperetail.fusion.orchestrator.adapter.in.messaging.jms;
 
+/*-
+ * *****
+ * fusion-orchestrator
+ * -----
+ * Copyright (C) 2018 - 2021 Scope Retail Systems Inc.
+ * -----
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * =====
+ */
+
 import static com.scoperetail.fusion.orchestrator.common.Event.OrderCreateViaJMS;
 import javax.xml.validation.Schema;
 import org.springframework.stereotype.Component;
@@ -26,17 +52,17 @@ public class OrderCreateTaskHandler extends AbstractMessageListener {
 
   @Override
   public void handleValidationFailure(final String event) throws Exception {
-    orderCreateUseCase.doValidationFailure(event);
+    orderCreateUseCase.createOrder(event, false);
   }
 
   @Override
   protected void handleMessage(final Object event) throws Exception {
-    orderCreateUseCase.doHandleMessage(event);
+    orderCreateUseCase.createOrder(event, true);
   }
 
   @Override
-  public void handleFailure(final String event) {
-    orderCreateUseCase.doHandleFailure(event);
+  public void handleFailure(final String message) {
+    orderCreateUseCase.send(getBoBrokerId(), getBoQueueName(), message);
   }
 
   @Override
--- src/main/java/com/scoperetail/fusion/orchestrator/application/service/command/OrderCreateService.java
+++ src/main/java/com/scoperetail/fusion/orchestrator/application/service/command/OrderCreateService.java
@@ -1,20 +1,46 @@
 package com.scoperetail.fusion.orchestrator.application.service.command;
 
+/*-
+ * *****
+ * fusion-orchestrator
+ * -----
+ * Copyright (C) 2018 - 2021 Scope Retail Systems Inc.
+ * -----
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * =====
+ */
+
 import static com.scoperetail.fusion.orchestrator.common.Event.OrderCreateViaJMS;
 import static com.scoperetail.fusion.orchestrator.common.Event.OrderCreateViaREST;
 import com.scoperetail.fusion.core.application.port.in.command.create.PosterUseCase;
+import com.scoperetail.fusion.core.application.port.out.jms.PosterOutboundJmsPort;
 import com.scoperetail.fusion.orchestrator.application.port.in.command.create.OrderCreateUseCase;
 import com.scoperetail.fusion.orchestrator.domain.OrderCreateRequest;
 import com.scoperetail.fusion.shared.kernel.common.annotation.UseCase;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @UseCase
 @AllArgsConstructor
-@Slf4j
 public class OrderCreateService implements OrderCreateUseCase {
 
   private PosterUseCase posterUseCase;
+  private PosterOutboundJmsPort posterOutboundJmsPort;
 
   @Override
   public void handleRestEvent(final OrderCreateRequest orderCreateRequest) throws Exception {
@@ -22,25 +48,12 @@ public class OrderCreateService implements OrderCreateUseCase {
   }
 
   @Override
-  public void doValidationFailure(final String event) {
-    try {
-      posterUseCase.post(OrderCreateViaJMS.name(), event, false);
-    } catch (final Exception e) {
-      log.error(
-          "An exception occured while executing doValidationFailure() for Usecase: {},  Exception: {}",
-          OrderCreateViaJMS.name(),
-          e);
-    }
+  public void createOrder(final Object event, final boolean isValid) throws Exception {
+    posterUseCase.post(OrderCreateViaJMS.name(), event, isValid);
   }
 
   @Override
-  public void doHandleMessage(final Object event) throws Exception {
-    posterUseCase.post(OrderCreateViaJMS.name(), event, true);
-  }
-
-  @Override
-  public void doHandleFailure(final String event) {
-    //BO
-    log.error("Unable to process event: {}", event);
+  public void send(final String brokerId, final String queueName, final String message) {
+    posterOutboundJmsPort.post(brokerId, queueName, message);
   }
 }
